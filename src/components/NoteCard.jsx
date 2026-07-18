1e43d6c11def1ae4b08409e3aedc19532421f15c
Shikharai100 <shikharai919@gmail.com>
2026-07-18 15:30:22 +0530

    Updated NoteCard component

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -3,7 +3,7 @@ import { X } from "lucide-react";
 
 const NoteCard = ({ note, idx, deleteNote }) => {
   return (
-    <div className='relative min-h-60 w-55 rounded-2xl text-black bg-cover bg-no-repeat bg-[url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYFUxpp7A6jYOhP1JWWtFBxZ8ULMzz7cRIkYMbikEmLeUQ9v2e4xfLZdke&s=10")]'>
+    <div className='relative min-h-70 w-55 rounded-2xl text-black bg-cover bg-no-repeat bg-[url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYFUxpp7A6jYOhP1JWWtFBxZ8ULMzz7cRIkYMbikEmLeUQ9v2e4xfLZdke&s=10")]'>
 
       <button
         onClick={() => deleteNote(idx)}
@@ -12,11 +12,11 @@ const NoteCard = ({ note, idx, deleteNote }) => {
         <X />
       </button>
 
-      <h2 className="leading-tight text-3xl font-bold pt-6 pl-5 break-all">
+      <h2 className="leading-tight text-2xl font-bold pt-6 pl-5 break-all">
         {note.title}
       </h2>
 
-      <p className="leading-tight px-4 py-3 text-lg">
+      <p className="leading-tight px-4 py-2 text-lg">
         {note.details}
       </p>
 
